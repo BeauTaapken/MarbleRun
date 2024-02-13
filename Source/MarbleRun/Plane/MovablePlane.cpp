6d80b37911aa948c5516ace7c1ab5139cdd10221
Beau <beau@lioncode.nl>
2024-02-13 17:14:53 +0100

    changed up some code so plane does not rotate weirdly when moving it and added a clamp

--- Source/MarbleRun/Plane/MovablePlane.cpp
+++ Source/MarbleRun/Plane/MovablePlane.cpp
@@ -19,20 +19,22 @@ void AMovablePlane::SetupPlayerInputComponent()
 		UE_LOG(LogTemp, Warning, TEXT("InputComponent was not set on MovablePlane"))
 		return;
 	}
-	InputComponent->BindAxis(TEXT("RotateDown"), this, &AMovablePlane::RotateDown);
-	InputComponent->BindAxis(TEXT("RotateRight"), this, &AMovablePlane::RotateRight);
+	InputComponent->BindAxis(FName(TEXT("RotateDown")), this, &AMovablePlane::RotateDown);
+	InputComponent->BindAxis(FName(TEXT("RotateRight")), this, &AMovablePlane::RotateRight);
 }
 
 void AMovablePlane::RotateDown(float AxisValue)
 {
-	FRotator Rotation = FRotator(1.0f * AxisValue, 0.0f, 0.0f);
-	AddActorWorldRotation(Rotation);
+	FRotator Rotation = this->GetActorRotation();
+	Rotation.Pitch = FMath::Clamp(Rotation.Pitch + (RotationSpeed * AxisValue), -40.0f, 40.0f);
+	this->SetActorRotation(Rotation, ETeleportType::TeleportPhysics);
 }
 
 void AMovablePlane::RotateRight(float AxisValue)
 {
-	FRotator Rotation = FRotator(0.0f, 0.0f, 1.0f * AxisValue);
-	AddActorWorldRotation(Rotation);
+	FRotator Rotation = this->GetActorRotation();
+	Rotation.Roll = FMath::Clamp(Rotation.Roll + (RotationSpeed * AxisValue), -40.0f, 40.0f);
+	this->SetActorRotation(Rotation, ETeleportType::TeleportPhysics);
 }
 
 
